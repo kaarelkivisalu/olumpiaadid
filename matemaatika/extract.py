afe583fa3f46257fd1f6a8699397069194ef32a8
randomdude999 <just.so.you.can.email.me@gmail.com>
2019-04-19 00:15:56 +0300

    matemaatikal ülesande kaupa punktid nüüd

--- matemaatika/extract.py
+++ matemaatika/extract.py
@@ -5,8 +5,16 @@ import json
 import collections
 import re
 
-data = collections.defaultdict(lambda: [])
+def extract_table(tbl_elem):
+    out_rows = []
+    for row in tbl_elem.find_all("tr"):
+        out_cols = []
+        for col in row.find_all("td"):
+            out_cols.append(col.get_text().strip())
+        out_rows.append(out_cols)
+    return out_rows
 
+data = collections.defaultdict(lambda: [])
 point_list = []
 
 other_class_markers = {
@@ -24,34 +32,47 @@ for x in os.listdir("pdf"):
         year = int(x.split("_")[0])
         def_cls = int(x.split("_")[1].split(".html")[0])
         soup = bs(f.read(), "lxml")
+        skipped_rows = 0
         i = 1
         prev_points = 0
         prev_placement = 1
-        for row in soup.table:
-            if isinstance(row, str):
+        tbl = extract_table(soup.table)
+        for row in tbl:
+            if row[0] == "Nimi" or row[0] == "" or row[0] == "Õpilase nimi":
+                skipped_rows += 1
                 continue
-            first_txt = row.td.get_text().strip()
-            if first_txt == "Nimi" or first_txt == "" or first_txt == "Õpilase nimi":
-                continue
-            tds = row.find_all("td")
-            name = tds[0].get_text().strip()
-            school = tds[1].get_text().strip()
-            if len(tds) == 9:
-                col_index = 7
-            elif len(tds) == 8:
-                col_index = 7
-            elif len(tds) == 10:
-                col_index = 8
+            name = row[0]
+            school = row[1]
+            if len(row) == 9:
+                total_index = 7
+            elif len(row) == 8:
+                total_index = 7
+            elif len(row) == 10:
+                total_index = 8
             else:
                 continue
+            ex_start_index = total_index - 5
             try:
-                if tds[col_index].get_text().strip() == "18+x":
+                if row[total_index] == "18+x":
                     points = 18
                 else:
-                    points = int(tds[col_index].get_text().strip())
+                    points = int(row[total_index])
             except:
-                print(x, i, tds[col_index].get_text())
+                print(x, i, row[total_index])
                 raise
+            ex_pts = []
+            for ex_col in range(ex_start_index, ex_start_index+5):
+                pts_str = row[ex_col]
+                if pts_str in ("", "-", "‒", "–","—", "x"):
+                    pts = 0
+                else:
+                    try:
+                        pts = int(pts_str)
+                    except:
+                        print(x, i, row[col_index])
+                        raise
+                ex_pts.append(pts)
+
             if points == prev_points:
                 placement = prev_placement
             else:
@@ -70,13 +91,29 @@ for x in os.listdir("pdf"):
                 cls = other_class_markers[x]
             elif name[-1] == '*':
                 name = name[:-1]
-            data[year].append({"name":name,"class":cls,"age_group":str(def_cls),"score":points,"school":school,"placement":placement})
+            data[year].append({
+                "name":name,
+                "class":cls,
+                "age_group":str(def_cls),
+                "score":points,
+                "school":school,
+                "placement":placement,
+                "scores_per_ex": ex_pts,
+            })
             i += 1
+    if skipped_rows > 3:
+        print(f"Warning: more than 3 rows skipped in {x}")
 
 for k,v in data.items():
     with open(f"{k}.json", 'w') as f:
         d = {
-            "age_groups": [{"name":str(x),"min_class":x,"max_class":x} for x in range(9, 13)],
+            "age_groups": [{
+                "name":str(x),
+                "min_class":x,
+                "max_class":x,
+                # matemaatika olümpiaadil on *alati* olnud 5 ülesannet
+                "ex_labels": ["1.","2.","3.","4.","5."]
+            } for x in range(9, 13)],
             "scores": v
         }
         json.dump(d, f)
